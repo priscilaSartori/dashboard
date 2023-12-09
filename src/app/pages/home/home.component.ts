d964044e7bab2573093660ec30c2cd4976f17ccc
Priscila Sartori <priscilasartori2@gmail.com>
2023-12-08 23:37:50 -0300

    feat: body

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from "../../components/header/header.component";
 import { SidenavComponent } from '../../components/sidenav/sidenav.component';
+import { BodyComponent } from '../../components/body/body.component';
 
 @Component({
     selector: 'app-home',
     standalone: true,
     templateUrl: './home.component.html',
     styleUrl: './home.component.scss',
-    imports: [HeaderComponent, SidenavComponent]
+    imports: [HeaderComponent, SidenavComponent, BodyComponent]
 })
 export class HomeComponent {
 

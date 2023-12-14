26ee9b91c74f7770afd1e36a52ba7f4c85611d7b
Priscila Sartori <priscilasartori2@gmail.com>
2023-12-14 00:10:40 -0300

    feat: top-widgets

--- src/app/components/body/body.component.ts
+++ src/app/components/body/body.component.ts
@@ -3,11 +3,12 @@ import { AgeComponent } from '../age/age.component';
 import { GenderComponent } from '../gender/gender.component';
 import { InterestComponent } from '../interest/interest.component';
 import { LocationComponent } from '../location/location.component';
+import { TopWidgetsComponent } from '../top-widgets/top-widgets.component';
 
 @Component({
   selector: 'app-body',
   standalone: true,
-  imports: [AgeComponent, GenderComponent, InterestComponent, LocationComponent],
+  imports: [AgeComponent, GenderComponent, InterestComponent, LocationComponent, TopWidgetsComponent],
   templateUrl: './body.component.html',
   styleUrl: './body.component.scss'
 })

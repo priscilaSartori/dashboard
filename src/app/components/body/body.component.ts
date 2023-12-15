c6db0f45f3c7a6beef93fe62cf73abd9b0a593ac
Priscila Sartori <priscilasartori2@gmail.com>
2023-12-15 19:35:49 -0300

    Revert "feat: top-widgets"
    This reverts commit 26ee9b91c74f7770afd1e36a52ba7f4c85611d7b.
    
    Revert "feat: top-widgets"

--- src/app/components/body/body.component.ts
+++ src/app/components/body/body.component.ts
@@ -3,12 +3,11 @@ import { AgeComponent } from '../age/age.component';
 import { GenderComponent } from '../gender/gender.component';
 import { InterestComponent } from '../interest/interest.component';
 import { LocationComponent } from '../location/location.component';
-import { TopWidgetsComponent } from '../top-widgets/top-widgets.component';
 
 @Component({
   selector: 'app-body',
   standalone: true,
-  imports: [AgeComponent, GenderComponent, InterestComponent, LocationComponent, TopWidgetsComponent],
+  imports: [AgeComponent, GenderComponent, InterestComponent, LocationComponent],
   templateUrl: './body.component.html',
   styleUrl: './body.component.scss'
 })
